334ebf0734f0b97c9ac999cfe64ec319e15df1ab
canbax <yusufsaidcanbaz@gmail.com>
2021-03-21 18:41:01 +0300

    code clean up

--- tgApi.py
+++ tgApi.py
@@ -7,18 +7,6 @@ with open('tg_conf.json', 'r') as f:
   CONF = json.loads(f.read())
 
 
-# def get_token():
-#   global TOKEN
-#   domain = CONF['db']['host']
-#   secret = CONF['db']['secret']
-#   url = f'{domain}:9000/requesttoken?secret={secret}'
-
-#   response = requests.request('GET', url, data={})
-#   resp = json.loads(response.text)
-#   TOKEN = resp['token']
-#   return TOKEN
-
-
 def run_interpretted_gsql(gsql: str):
   domain = CONF['db']['host']
   url = f'{domain}:14240/gsqlserver/interpreted_query'
@@ -30,10 +18,3 @@ def run_interpretted_gsql(gsql: str):
   if obj['error']:
     raise Exception(obj['message'])
   return obj['results']
-
-  # WORKING CODE !!!!
-  # data = 'INTERPRET QUERY () FOR GRAPH MyGraph { print "heloo"; }'
-  # response = requests.post('https://customer360.i.tgcloud.io:14240/gsqlserver/interpreted_query', data=data, auth=('tigergraph', '123456'))
-  # print(response.text)
-
-# get_token()
